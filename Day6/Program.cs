9fb29e05b4f1fa5d5526e5668803c0566dd71c67
DanielVelinrb <daniel.velin.2001@hotmail.com>
2023-06-25 02:16:46 -0500

    Dia 6

---
+++ Day6/Program.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+
+namespace Day6{
+    class Program{
+        static void Main(string [] args){
+
+            int num = int.Parse(ReadLine().ToString());
+            int [,] espiral = new int[num, num];
+            int contador = num * num;
+            int i = 0, j = num;
+            int direccion = 0;
+            int extension = num;
+            
+            while(contador > 0){                    
+                if(extension == num && j == 0){
+                    direccion++;
+                    extension--;
+                }
+                else if((j == num - extension && direccion == 0)){
+                    extension--;
+                    direccion++;
+                }
+                else if((i == extension && direccion == 1) || (j == extension && direccion == 2)){
+                    direccion++;
+                }
+                else if(i == num - extension && direccion == 3){
+                    direccion = 0;
+                }
+
+                switch(direccion){
+                    case 0:
+                        espiral[i, --j] = contador--; break;           
+                    case 1:
+                        espiral[++i, j] = contador--; break;
+                    case 2:
+                        espiral[i, ++j] = contador--; break;
+                    case 3:
+                        espiral[--i, j] = contador--; break;
+                }
+            }
+
+            for(i = 0; i < num; i++){
+                for(j = 0; j < num; j++){
+                    Write($"{espiral[i, j]} ");       
+                }
+                WriteLine("");
+            }
+        }
+    }
+}
