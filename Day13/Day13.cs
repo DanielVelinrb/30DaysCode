c83de5cce0b57ea53991cdf418da1f72d1ea3f28
DanielVelinrb <daniel.velin.2001@hotmail.com>
2023-07-01 22:58:46 -0500

    Dia 13

---
+++ Day13/Day13.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace Day13{
+    class Day13{    
+        static void Main(string [] args){
+
+            string entrada = "(0,0)";
+            long [] values = new long[2]{0,0};
+            long [] tempValues = new long[2]{0,0};
+
+            do{
+                WriteLine("Ingrese la coordenada con el formato (n,n)");
+                entrada = ReadLine();
+                if(Regex.IsMatch(entrada, @"^(\(-?\d+,-?\d+\))$")){
+                    entrada = Regex.Replace(entrada , @"\(|\)" , "");
+                    tempValues = entrada.Split(",").Select(number => long.Parse(number)).ToArray();
+                    values[0] += tempValues[0];
+                    values[1] += tempValues[1];
+                }
+                else
+                    break;
+            }while(true);
+
+            WriteLine("Lectura de datos finalizada");
+            WriteLine($"Posición final: ({values[0]} , {values[1]})");
+        }
+    }
+
+}
