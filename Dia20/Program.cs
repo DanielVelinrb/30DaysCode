b32a4bc335a634c0c4289fd28ed3506adaf7a3f3
DanielVelinrb <daniel.velin.2001@hotmail.com>
2023-07-08 22:21:18 -0500

    Dia 20

---
+++ Dia20/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Dia20{
+    class Dia20{
+        static void Main(string [] args){
+            int [] numeros = new int[]{0, 1, 2, 3, 4, 5, 7, 8, 9, 10};
+            int n = numeros.Length + 1;
+            int sumatoria = n * (numeros[0] + numeros[n - 2])/2;
+            WriteLine($"Numero faltante: {sumatoria - numeros.Sum()}");
+        }
+    }
+}
